2b5ca6407dd2e80fc613bcf83abef8d968864753
Andy Pilay <pilayandy2001@gmail.com>
2023-02-22 00:24:00 -0500

    new donaciones

---
+++ controllers/donaciones.php
@@ -0,0 +1,84 @@
+<?php
+require_once "../config/respuesta.class.php";
+require_once "../models/donaciones.class.php";
+require_once "../config/permisos.class.php";
+
+$_encabezado = new PermisosEncabezados;
+$_encabezado->PermisosEncabezados();
+
+$_respuestas = new respuestas;
+$_Donaciones = new Donaciones;
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    if (isset($_GET["page"])) {
+        $pagina = $_GET["page"];
+        $listarDonaciones= $_Donaciones->listarDonaciones($pagina);
+        header("Content-Type: application/json");
+        echo json_encode($listarDonaciones);
+        http_response_code(200);
+    } 
+
+    else if (isset($_GET["id"])) {
+        $listarDonacionesID = $_GET["id"];
+        $datoslistarDonaciones = $_Donaciones->obtenerIDonaciones($_DonacionesID);
+        header("Content-Type: application/json");
+        echo json_encode($datosDonaciones);
+        http_response_code(200);
+    }
+
+}
+else if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    #recibir los datos enviados
+    $postBody = file_get_contents("php://input");
+    #enviamos los datos al manejador
+    $datosArray = $_Donaciones->post($postBody);
+
+    //devolvemos una respuestas
+    header('Content-Type: application/json');
+    if (isset($datosArray["result"]["error_id"])) {
+        $responseCode = $datosArray["result"]["error_id"];
+        http_response_code($responseCode);
+    } else {
+        http_response_code(200);
+    }
+    echo json_encode($datosArray);
+}
+else if ($_SERVER['REQUEST_METHOD'] == "PUT") {
+    //recibimos los datos enviados
+    $postBody = file_get_contents("php://input");
+    //enviamos datos al manejador
+    $datosArray = $_Donaciones->put($postBody);
+    //delvovemos una respuesta 
+
+    header('Content-Type: application/json');
+    if (isset($datosArray["result"]["error_id"])) {
+        $responseCode = $datosArray["result"]["error_id"];
+        http_response_code($responseCode);
+    } else {
+        http_response_code(200);
+    }
+    echo json_encode($datosArray);
+} 
+else if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
+    //recibimos los datos enviados
+    $postBody = file_get_contents("php://input");
+    //enviamos datos al manejador
+    $datosArray = $_Donaciones->delete($postBody);
+    //delvovemos una respuesta 
+
+    header('Content-Type: application/json');
+    if (isset($datosArray["result"]["error_id"])) {
+        $responseCode = $datosArray["result"]["error_id"];
+        http_response_code($responseCode);
+    } else {
+        http_response_code(200);
+    }
+    echo json_encode($datosArray);
+} 
+
+else {
+    header("Content-Type: application/json");
+    $datosArray = $__respuesta->error_405;
+    echo json_encode($datosArray);
+}
---
+++ models/donaciones.class.php
@@ -0,0 +1,175 @@
+<?php
+require_once "../config/conexion/conexion.php";
+require_once "../config/respuesta.class.php";
+require_once "../config/Logs.php";
+
+class Donaciones extends conexion
+{
+    private $tabla = "tbl_donaciones";
+    private $id_donacion = "";
+    private $type_products = "";
+    private $quantity = "";
+    private $description = "";
+    private $status = "";
+    private $date_creation = "";
+
+    public function listarDonaciones($pagina = 1)
+    {
+        $inicio = 0;
+        $cantidad = 100;
+
+        if ($pagina > 1) {
+            $inicio = $cantidad * ($pagina - 1) + 1;
+            $cantidad = $cantidad * $pagina;
+        }
+
+        $query = "select * from $this->tabla limit $inicio,$cantidad";
+        //print_r($query);
+        $datos = parent::obtenerDatos($query);
+        return $datos;
+    }
+
+
+    public function obtenerIDonaciones($id)
+    {
+        $query = "select * from $this->tabla where id_user  = $id";
+        return $datos = parent::obtenerDatos($query);
+    }
+
+    /* POST */
+
+
+    public function post($json)
+    {
+        $_respuestas = new respuestas;
+        $datos = json_decode($json, true);
+        if (!isset($datos["type_products"]) || !isset($datos["quantity"])) {
+            return $_respuestas->error_400();
+        } else {
+            $this->type_products = $datos['type_products'];
+            $this->quantity = $datos['quantity'];
+            $this->description = $datos['description'];
+            $this->status = $datos['status'];
+            $this->date_creation = $datos['date_creation'];
+
+            $resp = $this->insertarDonaciones();
+
+            if ($resp) {
+                $respuesta = $_respuestas->response;
+                $respuesta["result"] = array(
+                    "DonacionesId" => $resp
+                );
+
+                return $respuesta;
+            } else {
+                return $_respuestas->error_500();
+            }
+        }
+    }
+
+
+    private function insertarDonaciones()
+    {
+        $query = "INSERT INTO $this->tabla(type_products, quantity, description,status,date_creation)
+         VALUES ('$this->type_products', '$this->quantity','$this->description','$this->status','$this->date_creation')";
+        $resp = parent::noQueryId($query);
+
+        if ($resp) {
+            return $resp;
+        } else {
+            return 0;
+        }
+    }
+
+    /* PuT */
+
+    public function put($json)
+    {
+        $_respuestas = new respuestas;
+        $datos = json_decode($json, true);
+        if (!isset($datos["id_donacion"]) || !isset($datos["type_products"])) {
+            return $_respuestas->error_400();
+        } else {
+            $this->id_donacion = $datos['id_donacion'];
+            $this->type_products = $datos['type_products'];
+            $this->quantity = $datos['quantity'];
+            $this->description = $datos['description'];
+            $this->status = $datos['status'];
+            $this->date_creation = $datos['date_creation'];
+
+            $resp = $this->actualizarDonaciones();
+
+            if ($resp) {
+                $respuesta = $_respuestas->response;
+                $respuesta["result"] = array(
+                    "id_information" => $resp
+                );
+
+                return $respuesta;
+            } else {
+                return $_respuestas->error_500();
+            }
+        }
+    }
+
+
+    private function actualizarDonaciones()
+    {
+        $query = "UPDATE `tbl_donaciones` SET `type_products`='$this->type_products', `quantity`='$this->quantity', `description`='$this->description', `status`='$this->status',
+        `date_creation`='$this->date_creation' WHERE `id_donacion` = '$this->id_donacion'";
+        print_r($query);
+        $resp = parent::noQuery($query);
+        if ($resp) {
+            return $resp;
+        } else {
+            return 0;
+        }
+    }
+
+
+    /* DELETE */
+
+
+    public function delete($json)
+    {
+        $_respuestas = new respuestas;
+        $datos = json_decode($json, true);
+
+        if (!isset($datos["id_donacion"])) {
+            return $_respuestas->error_400();
+        } else {
+
+            $this->id_donacion = $datos['id_donacion'];
+
+            $resp = $this->eliminarDonaciones();
+
+            if ($resp) {
+                $respuesta = $_respuestas->response;
+                $respuesta["result"] = array(
+                    "id_donacion" => $this->id_donacion
+                );
+
+                return $respuesta;
+            } else {
+                return $_respuestas->error_500();
+            }
+        }
+    }
+
+
+
+
+    private function eliminarDonaciones()
+    {
+        $query = "DELETE FROM $this->tabla WHERE id_donacion='$this->id_donacion'";
+
+        $resp = parent::noQuery($query);
+
+        if ($resp >= 1) {
+            return $resp;
+        } else {
+            return 0;
+        }
+    }
+}
+
