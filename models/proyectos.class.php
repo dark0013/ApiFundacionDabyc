eac3534f2fa50261710d8dd038caa015778e0340
Andy Pilay <pilayandy2001@gmail.com>
2023-03-19 12:57:16 -0500

    update

--- models/proyectos.class.php
+++ models/proyectos.class.php
@@ -10,9 +10,8 @@ class Proyectos extends conexion
     private $id_project = "";
     private $title = "";
     private $url_image = "";
-    private $image = "";
     private $description = "";
-    private $rol_user = "";
+    private $date_proyect = "";
     private $status = "";
     private $user_sesion = "";
     private $date_creation = "";
@@ -74,9 +73,8 @@ class Proyectos extends conexion
             $this->url_image =  $this->procesarImagen($datos['url_image']);
 
 
-            $this->image = $datos['image'];
             $this->description = $datos['description'];
-            $this->rol_user = $datos['rol_user'];
+            $this->date_proyect = $datos['date_proyect'];
             $this->status = $datos['status'];
             $this->user_sesion = $datos['user_sesion'];
             $this->date_creation = $datos['date_creation'];
@@ -100,8 +98,8 @@ class Proyectos extends conexion
 
     private function insertarProyectos()
     {
-        $query = "INSERT INTO `tbl_proyectos`(title, url_image, image,  description, rol_user,user_sesion,date_creation,user_creation)
-        VALUES ('$this->title', '$this->url_image','$this->image','$this->description', '$this->rol_user', '$this->user_sesion', '$this->date_creation', '$this->user_creation')";
+        $query = "INSERT INTO `tbl_proyectos`(title, url_image,  description,date_proyect, status,user_sesion,date_creation,user_creation)
+        VALUES ('$this->title','$this->url_image','$this->description', '$this->date_proyect','$this->status','$this->user_sesion', '$this->date_creation', '$this->user_creation')";
         $resp = parent::noQueryId($query);
 
         if ($resp) {
@@ -124,34 +122,31 @@ class Proyectos extends conexion
             $this->id_project = $datos['id_project'];
             $this->title = $datos['title'];
             $this->url_image =  $this->procesarImagen($datos['url_image']);
+            $this->date_proyect = $datos['date_proyect'];
+            $this->date_update = $datos['date_update'];
             $this->description = $datos['description'];
-            //$this->rol_user = $datos['rol_user'];
-            $this->status = $datos['status'];
             $this->user_sesion = $datos['user_sesion'];
-            $this->date_creation = $datos['date_creation'];
-            //$this->date_update = $datos['date_update'];
-            //$this->user_creation = $datos['user_creation'];
             $this->user_update = $datos['user_update'];
-
+    
             $resp = $this->actualizarProyectos();
-            echo $resp;
             if ($resp) {
                 $respuesta = $_respuestas->response;
                 $respuesta["result"] = array(
-                    "id_project" => $resp
+                    "id_project" => $this->id_project
                 );
-
+    
                 return $respuesta;
             } else {
                 return $_respuestas->error_500();
             }
         }
     }
-
-
+    
+    
     private function actualizarProyectos()
     {
-        $query = "UPDATE `tbl_proyectos` SET `title` = '$this->title',`url_image` = '$this->url_image',`description`='$this->description', `user_sesion`='$this->user_sesion', `date_update`='$this->date_creation', `date_creation`='$this->date_creation',`user_update`='$this->user_update', `status`= '$this->status' WHERE `id_project` = '$this->id_project'";
+        $query = "UPDATE `tbl_proyectos` SET `title` = '$this->title',`url_image` = '$this->url_image',`description`='$this->description', 
+        `date_proyect`='$this->date_proyect',`user_sesion`='$this->user_sesion', `date_update`='$this->date_update', `user_update`='$this->user_update' WHERE `id_project` = '$this->id_project'";
         $resp = parent::noQuery($query);
         if ($resp) {
             return $resp;
@@ -159,6 +154,7 @@ class Proyectos extends conexion
             return 0;
         }
     }
+    
 
     /* DELETE */
 
@@ -211,7 +207,10 @@ class Proyectos extends conexion
     {
         // Obtener la ruta base de la aplicación en el servidor
         $ruta_base = $_SERVER['DOCUMENT_ROOT'] . '/ApiFundacionDabyc/public/imagenes/';
-    
+         // Verificar si la carpeta "imagenes" existe, si no, crearla
+        if (!file_exists($ruta_base)) {
+            mkdir($ruta_base, 0777, true);
+        }
         // Obtener la extensión de la imagen a partir del tipo MIME
         $partes = explode(";base64,", $imagen);
         $mime_type = $partes[0];
